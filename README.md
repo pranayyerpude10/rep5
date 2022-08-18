b7be2eeb63d15534cb08304806b3812c08ba768d
pranayyerpude10 <83808067+pranayyerpude10@users.noreply.github.com>
2022-08-18 12:09:46 +0530

    hi

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ibtaie
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ibtaie)
---
+++ src/app/productpage/productpage.component.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../service/Dataproduct.service';
+import { Product } from './product';
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent implements OnInit {
+  products: Array<Product> = [];
+  constructor(private product: DataService, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.product.getproducts().subscribe((data: any[]) => {
+      this.products = (data as any).products;
+      console.log(this.products);
+      console.log(data);
+    });
+  }
+}
